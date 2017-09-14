a1430996a0ecea69b8d26363a3dc6b5517cdc30c
Greg doud <gpdoud@gmail.com>
2017-09-14 15:20:01 -0400

    Initial commit

--- TestAutomatedCodeTesting/Program.cs
+++ TestAutomatedCodeTesting/Program.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AutomatedCodeTesting {
+namespace TestAutomatedCodeTesting {
 	class Program {
+
+		void Run() {
+
+		}
+
 		static void Main(string[] args) {
+			new Program().Run();
 		}
 	}
 }
